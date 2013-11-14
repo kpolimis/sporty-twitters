4a0dc63869ba6163a10fe3cdcdde3b0b34a92a15
Virgile Landeiro <vlandeir@hawk.iit.edu>
2013-11-14 00:45:21 -0600

    Enhance progress bar and finish scripts that gather friends and tweets of an user

--- src/user.py
+++ src/user.py
@@ -18,6 +18,8 @@ parser.add_argument('-d', type=str, help='directory where the user files will be
 parser.add_argument('-N', type=int, help='number of tweets to collect for each user', metavar='number-tweets', default=200)
 parser.add_argument('-s', type=str, help='json file containing the Twitter API key and token', metavar='settings-file', default='settings.json')
 parser.add_argument('-act', choices=['tweets', 'friends', 'both'], help='define which action will be executed by the program', default='tweets')
+parser.add_argument('-lb', type=int, help='lower bound for the processed user ids',default=-1)
+parser.add_argument('-ub', type=int, help='upper bound for the processed user ids',default=-1)
 
 if __name__ == "__main__":
 	args = parser.parse_args()
@@ -41,30 +43,44 @@ if __name__ == "__main__":
 
 	# Load the list of known users
 	users = json.load(open(args.i))
+	users = sorted(users, key=int)
+
+	restricted_users = []
+	for u in users:
+		u = int(u)
+		if (args.lb == -1 or u > args.lb) and (args.ub == -1 or u < args.ub):
+			restricted_users.append(u)
+	users = restricted_users
+
+	print users
 	bar = ProgressBar(30)
 	max_users = len(users)
 	actual = 0
 
 	for u in users:
+		#print actual, max_users
+		u = int(u)
 		actual += 1
-		user_directory = "./" + args.d + "/" + u
+		user_directory = "./" + args.d + "/" + str(u)
 		ensure_dir(user_directory)
-		#print "-----" + str(u) + "-----"
 		try:
 			if args.act == 'tweets' or args.act == 'both':
 				tweets_file = open(user_directory + "/tweets.json", "w")
 				for tweet in tweepy.Cursor(api.user_timeline, id=u).items(args.N):
 					tweets_file.write(tweet.text + "\n")
 				tweets_file.close()
+			if args.act == 'friends' or args.act == 'both':
+				friends_file = open(user_directory + "/friends.json", "w")
+				user = api.get_user(id=u)
+				friends = api.friends_ids(id=user.id)
+				followers = api.followers_ids(id=user.id)
+				for userid in friends:
+					if userid in followers:
+						friends_file.write(str(userid) + "\n")
+				friends_file.close()
 		except UnicodeEncodeError:
 			continue
 		except tweepy.error.TweepError:
-			# user tweets are not public, delete his foldeer
+			# user tweets are not public, delete his folder
 			continue
 		bar.update(float(actual)/float(max_users))
-    #if args.act == 'friends' or args.act == 'both':
-	# ids = []
-	# for page in tweepy.Cursor(api.followers_ids, screen_name="McDonalds").pages():
-	#     ids.extend(page)
-	#     print page
-	#     time.sleep(60)
