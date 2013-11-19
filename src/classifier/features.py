a1b33ce37941880c7db9442bc17f488ac163d62b
Virgile Landeiro Dos Reis <vlandeir@hawk.iit.edu>
2013-11-19 00:31:43 -0600

    Changed features.py in order to integrate bigrams (unstable with other files)

--- src/classifier/features.py
+++ src/classifier/features.py
@@ -1,15 +1,31 @@
 import nltk
-
+	
 def get_words_in_tweets(tweets):
-        all_words = []
-        for (words, sentiment) in tweets:
-                all_words.extend(words)
-        return all_words
+	all_words = []
+	for (words, sentiment) in tweets:
+		all_words.extend(words)
+	return all_words
 
-def get_word_features(wordlist, cutoff):
-        wordlist = nltk.FreqDist(wordlist)
-        word_features = wordlist.keys()
-        return word_features[:cutoff]
+def get_bigrams_in_tweets(tweets):
+	all_bigrams = []
+	for (words, sentiment) in tweets:
+		all_bigrams.extend(nltk.bigrams(words))
+	return all_bigrams
 
-def tweets2features(tweets, cutoff):
-        return get_word_features(get_words_in_tweets(tweets), cutoff)
+def tweets2features(tweets, bigrams=False, cutoff=3000, split=0.9):
+	features = {'unigrams':[], 'bigrams':[]}
+	cutoff_unigrams = cutoff*split
+	cutoff_bigrams = cutoff-cutoff_unigrams
+
+	# Getting unigrams
+	worldist = get_words_in_tweets(tweets)
+	wordlist = nltk.FreqDist(wordlist)
+	features['unigrams'] = wordlist.keys()[:cutoff_unigrams]
+
+	# Getting bigrams
+	bigramslist = get_bigrams_in_tweets(tweets)
+	bigramslist = nltk.FreqDist(bigramslist)
+	features['bigrams'] = bigramslist.keys()[:cutoff_bigrams]
+
+	return features
+
