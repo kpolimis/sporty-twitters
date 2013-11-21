3afa1132bcb95df7509c416451ac986d2d92e0f3
Virgile Landeiro Dos Reis <vlandeir@hawk.iit.edu>
2013-11-20 19:50:30 -0600

    add classifier files

--- src/classifier/features.py
+++ src/classifier/features.py
@@ -12,18 +12,23 @@ def get_bigrams_in_tweets(tweets):
 		all_bigrams.extend(nltk.bigrams(words))
 	return all_bigrams
 
-def tweets2features(tweets, bigrams=False, cutoff=3000, split=0.1):
-	cutoff_unigrams = int(cutoff*split)
-	cutoff_bigrams = int(cutoff-cutoff_unigrams)
+def tweets2features(tweets, bigrams=False, cutoff=3000, split=0.75):
+	if bigrams:
+		cutoff_unigrams = int(cutoff*split)
+		cutoff_bigrams = int(cutoff-cutoff_unigrams)
+	else:
+		cutoff_unigrams = cutoff
 
 	# Getting unigrams
 	wordlist = get_words_in_tweets(tweets)
 	wordlist = nltk.FreqDist(wordlist)
 	unigram_feats = wordlist.keys()[:cutoff_unigrams]
 
-	# Getting bigrams
-	bigramslist = get_bigrams_in_tweets(tweets)
-	bigramslist = nltk.FreqDist(bigramslist)
-	bigram_feats = bigramslist.keys()[:cutoff_bigrams]
+	bigram_feats = []
+	if bigrams:
+		# Getting bigrams
+		bigramslist = get_bigrams_in_tweets(tweets)
+		bigramslist = nltk.FreqDist(bigramslist)
+		bigram_feats = bigramslist.keys()[:cutoff_bigrams]
 
 	return unigram_feats + bigram_feats
