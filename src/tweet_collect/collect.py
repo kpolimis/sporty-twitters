b96df01805070425993c40ec1241fc3bfbfa0dc4
Virgile Landeiro Dos Reis <vlandeir@hawk.iit.edu>
2014-04-23 11:04:55 -0500

    add utf8 support

--- src/tweet_collect/collect.py
+++ src/tweet_collect/collect.py
@@ -5,7 +5,6 @@ from collections import defaultdict
 import codecs
 import sys
 
-sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
 def authenticate(settings_file):
 	settings = json.load(open(settings_file, 'r'))
@@ -26,24 +25,30 @@ def files_to_track(files):
 				tracked_words.append(line.strip())
 	return tracked_words
 
-def collect(tracked_words, limit=10, lang=["en-EN", "en", "en-CA", "en-GB"], locations=None):
+def collect(tracked_words, output=sys.stdout, count=0, lang=["en-EN", "en", "en-CA", "en-GB"], locations=None):
+	i = 0
+	out = codecs.getwriter('utf8')(output)
+
 	req_options = dict()
 	req_options['track'] = ",".join(tracked_words)
 	req_options['language'] = ",".join(lang)
 	if locations:
 		req_options['locations'] = locations
 
-	count_tracked_words = {tr:0 for tr in tracked_words}
-
 	r = api.request('statuses/filter', req_options)
 	for item in r.get_iterator():
-		print json.dumps(item)
-		sys.stdout.flush()
+		if 'limit' not in item.keys():
+			out.write(json.dumps(item))
+			out.write("\n")
+			out.flush()
+			i += 1
+			if count and i >= count:
+				break
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--settings", "-s", dest='settings', type=str)
-	parser.add_argument("--limit", "-l", type=int, default=0)
+	parser.add_argument("--count", "-c", type=int, default=0)
 	parser.add_argument("-f", "--file", dest='fileflag', action='store_true')
 	parser.add_argument("track", type=str, nargs='+')
 	args = parser.parse_args()
@@ -51,4 +56,5 @@ if __name__ == "__main__":
 	tracked_words = (args.fileflag and files_to_track(args.track)) or args.track
 	
 	api = authenticate(args.settings)
-	collect(tracked_words, args.limit)
+	collect(tracked_words, count=args.count)
+    #tags2track = ['runkeeper','nikeplus','runtastic','endomondo','mapmyrun','strava','cyclemeter','fitstats','mapmyfitness','runmeter']
