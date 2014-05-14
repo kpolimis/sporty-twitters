456938b171e7f2d4fc8619d48abed65865269117
Virgile Landeiro Dos Reis <vlandeir@hawk.iit.edu>
2014-05-14 11:42:31 -0500

    improved clean_corpus with removing of repeated characters more than 2 times. finished build features script

--- src/utils/clean_corpus.py
+++ src/utils/clean_corpus.py
@@ -6,13 +6,18 @@ import string
 import codecs
 import json
 
-urlregex = re.compile(r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`()\[\]{};:'"<>?]))''')
-mentionsregex = re.compile(r'''(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)''')
+url_regex = re.compile(r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`()\[\]{};:'"<>?]))''')
+mentions_regex = re.compile(r'''(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)''')
+unicode_regex = re.compile(r'''[^\x00-\x7F]''')
+stem_regex = re.compile(r'''([a-zA-Z])\1{2,}''', re.DOTALL)
 exclude = set(string.punctuation)
 
-def preprocess(line, rm_urls=True, rm_mentions=True, rm_punctuation=True, raw_json=False):
-    """Process a string in order to remove urls, mentions, and punctuation."""
-
+def preprocess(line, rm_urls=True, rm_mentions=True, rm_punctuation=True, rm_unicode=True, raw_json=False):
+    """
+    Processes a string in order to remove urls, mentions, unicode characters,
+    and punctuation. It also replaces characters that are repeated more than
+    three times by two of the same character.
+    """
     if raw_json:
         tw = json.loads(line)
         line = tw['text'].lower()
@@ -20,27 +25,31 @@ def preprocess(line, rm_urls=True, rm_mentions=True, rm_punctuation=True, raw_js
         line = line.lower()
 
     if rm_mentions:
-        line = re.sub(mentionsregex, '', line) 
+        line = re.sub(mentions_regex, '', line) 
 
     if rm_urls:
-        line = re.sub(urlregex, '', line) 
+        line = re.sub(url_regex, '', line) 
     
+    if rm_unicode:
+        line = re.sub(unicode_regex, '', line)
+
     if rm_punctuation:
         line = ''.join(c for c in line if c not in exclude)
+
+    line = re.sub(stem_regex, r'\1\1', line)
 
     return line
 
 def tokenize(line, stopwords):
     """Tokenize a string and the remove given stopwords from the final list of words"""
-    word = re.split("[\s]+", line)
-    word = [x for x in word if x] # remove starting space
+    words = re.split("[\s]+", line)
+    words = [x for x in words if x] # remove starting space
     if stopwords:
         # remove stopwords
-        word = [x for x in word if x not in stopwords]
-    line = " ".join(word)
-    return line
+        words = [x for x in words if x not in stopwords]
+    return words
 
-def clean(in_stream=sys.stdin, out_stream=sys.stdout, stopwords=[], rm_urls=True, rm_mentions=True, rm_punctuation=True, raw_json=False):
+def clean(in_stream=sys.stdin, out_stream=sys.stdout, stopwords=False, rm_urls=True, rm_mentions=True, rm_punctuation=True, rm_unicode=True, raw_json=False):
     """Clean a corpus given as an input stream by removing stopwords, URLs, mentions, and punctuation."""
     out_stream = codecs.getwriter('utf8')(out_stream)
 
@@ -50,8 +59,9 @@ def clean(in_stream=sys.stdin, out_stream=sys.stdout, stopwords=[], rm_urls=True
             sw = set(x.strip() for x in sw_file)
 
     for line in in_stream:
-        line = preprocess(line, rm_urls, rm_mentions, rm_punctuation, raw_json)
-        line = tokenize(line, sw)
+        line = preprocess(line, rm_urls, rm_mentions, rm_punctuation, rm_unicode, raw_json)
+        words = tokenize(line, sw)
+        line = " ".join(words)
 
         # write result in the output stream
         if line:
@@ -60,15 +70,15 @@ def clean(in_stream=sys.stdin, out_stream=sys.stdout, stopwords=[], rm_urls=True
                 line = json.dumps(tw)
             out_stream.write(line + "\n")
 
-"""Remove given stopwords, URLs, and punctuation from a corpus of raw tweets."""
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("stopwords", type=str, default="None", nargs='?')
-    parser.add_argument("--keep-mentions", dest='rm_mentions', action='store_false')
-    parser.add_argument("--keep-urls", dest='rm_urls', action='store_false')
-    parser.add_argument("--keep-punctuation", dest='rm_punctuation', action='store_false')
+    parser.add_argument("--keep-mentions", "-m", dest='rm_mentions', action='store_false')
+    parser.add_argument("--keep-urls", "l", dest='rm_urls', action='store_false')
+    parser.add_argument("--keep-punctuation", "-p", dest='rm_punctuation', action='store_false')    
+    parser.add_argument("--keep-unicode", "-u", dest='rm_unicode', action='store_false')
     parser.add_argument("--raw-json", "--json", dest='raw_json', action='store_true')
-    parser.set_defaults(rm_mentions=True, rm_stopwords=True, rm_urls=True, rm_punctuation=True, raw_json=False)
+    parser.set_defaults(rm_mentions=True, rm_stopwords=True, rm_urls=True, rm_punctuation=True, rm_unicode=True, raw_json=False)
     args = parser.parse_args()
 
-    clean(sys.stdin, sys.stdout, args.stopwords, args.rm_mentions, args.rm_urls, args.rm_punctuation, args.raw_json)
+    clean(sys.stdin, sys.stdout, args.stopwords, args.rm_mentions, args.rm_urls, args.rm_punctuation, args.rm_unicode, args.raw_json)
