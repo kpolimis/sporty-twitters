a57a8aef7509bd3ff9c5d1a7332138bb2070a957
Virgile Landeiro Dos Reis <vlandeir@hawk.iit.edu>
2014-05-28 09:41:30 -0500

    add benchmark functionality to CLI

--- src/cli.py
+++ src/cli.py
@@ -3,13 +3,15 @@ import sporty.utils as utils
 from sporty.datastructures import *
 import argparse
 import sys
+from sklearn.svm import SVC
+from sklearn.multiclass import OneVsRestClassifier
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Command Line Interface for the sporty twitter project')
     # list of actions
     parser.add_argument('action',
                         type=str,
-                        choices=['collect', 'label'],
+                        choices=['collect', 'label', 'benchmark'],
                         default='collect',
                         help='action to execute (default: collect)')
     # input(s)
@@ -48,6 +50,16 @@ if __name__ == "__main__":
                              type=int,
                              help='line number to begin labeling',
                              default=0)
+    # benchmark group
+    benchmark_group = parser.add_argument_group('Benchmark a classifier')
+    benchmark_group.add_argument('--stopwords', '-sw',
+                                 type=file,
+                                 help='path to the stopwords file',
+                                 default=None)
+    benchmark_group.add_argument('--min-df', 
+                                 dest='mindf',
+                                 type=int,
+                                 default=5)
     # # filter group
     # filter_group = parser.add_argument_group('Get N tweets amongst a bulk of tweets by filtering on keywords')
     # filter_group.add_argument('n', type=int, default=10,
@@ -73,5 +85,23 @@ if __name__ == "__main__":
             api.load(input_file)
             labels = {l:args.lv for l in args.ln}
             api.label(labels, output_file, args.begin)
+        else:
+            raise Exception("Label name(s) and values are required when labeling tweets.")
+    elif args.action == 'benchmark':
+        if args.ln and args.lv:
+            api = sporty.api()
+            input_file = args.input[0]
+            output_file = args.output[0]
+            labels = {l:args.lv for l in args.ln}
+            if len(labels.keys()) > 1:
+                api.mood.clf = OneVsRestClassifier(SVC(kernel='linear'))
+            tweets = Tweets(input_file)
+            corpus = utils.Cleaner(stopwords=LSF(args.stopwords).tolist()).clean(tweets)
+
+            tfidf_options = {'min_df': args.mindf}
+            api.buildFeatures(corpus, labels=labels.keys())
+            api.buildVectorizer()
+            api.train()
+            api.benchmark(cv=2)
         else:
             raise Exception("Label name(s) and values are required when labeling tweets.")
