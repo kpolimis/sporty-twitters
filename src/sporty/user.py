ef3ebaee0d21e9a21be7332380425c6468944113
Virgile Landeiro Dos Reis <vlandeir@hawk.iit.edu>
2014-06-02 16:41:26 -0500

    add user api to collect tweets of a uniquer user

---
+++ src/sporty/user.py
@@ -0,0 +1,36 @@
+import json
+from tweets import Tweets
+from utils import TwitterAPIUser
+import time
+
+class User(TwitterAPIUser):
+    """docstring for User"""
+    def __init__(self, user_id, settings_file=None):
+        super(User, self).__init__(settings_file)
+        self.user_id = user_id
+        self.tweets = Tweets()
+
+    def collect_tweets(self, count=3200, output_file=None, mode='a+'):
+        self.tweets = Tweets(output_file, mode)
+        i = 0
+        max_id = 10
+        while True:
+            try:
+                r = self.getUserStream(self.user_id, max_id=max_id)
+                if not r.get_iterator().results:
+                    return
+                for item in r.get_iterator():
+                    if 'message' in item.keys():
+                        remaining = r.get_rest_quota()['remaining']
+                        if not remaining:
+                            sleep_sec = 60
+                            time.sleep(sleep_sec)
+                    else:
+                        max_id = item['id']
+                        self.tweets.append(item)
+                        i += 1
+                        if count and i >= count:
+                            break
+            except Exception, e:
+                raise e
+
