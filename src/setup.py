512650a9b8907ec8dcc78f12bf3a7a094a83a32a
Virgile Landeiro Dos Reis <vlandeir@hawk.iit.edu>
2014-06-04 16:23:19 -0500

    add setup.py

---
+++ src/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='Sporty Twitters',
+      version='0.1',
+      description='Source code to the sporty twitters project looking to correlate the amount of exercise of a Twitter user with its well-being.',
+      author='Virgile Landeiro',
+      author_email='vlandeir@hawk.iit.edu',
+      url='https://github.com/vlandeiro/sporty-twitters',
+      packages=['sporty'],
+      py_modules=['cli'],
+      requires=['nltk', 'sklearn', 'TwitterAPI', 'docopt'],
+      entry_points={
+        'console_scripts': [
+            'sporty = cli:main'
+        ]}
+      )
