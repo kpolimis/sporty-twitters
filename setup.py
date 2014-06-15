1aa2f1d52f61e0b9650c532cd5931ede7c97b584
Virgile Landeiro <vlandeir@hawk.iit.edu>
2014-06-15 12:59:44 -0500

    update setup.py to remove nltk dependency

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='Sporty Twitters',
       packages=['sporty', 'cli'],
       package_dir={'sporty': 'src/sporty', 'cli': 'src/cli'},
       py_modules=['sporty.mood', 'sporty.sporty', 'sporty.tweets', 'sporty.user', 'sporty.utils'],
-      install_requires=['nltk', 'TwitterAPI', 'docopt', 'scipy', 'numpy', 'scikit-learn'],
+      install_requires=['TwitterAPI', 'docopt', 'scipy', 'numpy', 'scikit-learn'],
       entry_points={'console_scripts': [
                     'sporty = cli.cli:main'
                     ]}
