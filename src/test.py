37d1514531ab5ce758a6e6c8027fc6390d35dbaa
Virgile Landeiro <vlandeir@hawk.iit.edu>
2014-07-11 11:24:31 -0500

    list all files to get all the ids in test.py

--- src/test.py
+++ src/test.py
@@ -1,13 +1,14 @@
 import sporty.users as u
-import glob
+from os import listdir
+from os.path import isfile, join
+import sys
 
-uids = [111397971,
-        1115884644,
-        111636637,
-        1118674309,
-        1120500210,
-        112091139,
-        112203950,
-        1122204780]
+user_dir = '/data/1/sporty/users/'
+friends_dir = '/data/1/sporty/friends/'
+uids = [f for f in listdir(user_dir) if isfile(join(user_dir,f))]
+
+#print uids
+#uids = [111397971,1115884644,111636637,1118674309,1120500210,112091139,112203950,1122204780]
+
 users = u.api(uids)
-users.getMostSimilarFriend('../inputs/users/', '../inputs/friends/')
+users.getMostSimilarFriend(user_dir, friends_dir)
