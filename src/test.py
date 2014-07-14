2daf88f3db8a10ff993e95a097f14e792cdf22f2
Virgile Landeiro <vlandeir@hawk.iit.edu>
2014-07-14 16:30:47 -0500

    slightly change test.py

--- src/test.py
+++ src/test.py
@@ -2,13 +2,23 @@ import sporty.users as u
 from os import listdir
 from os.path import isfile, join
 import sys
+import re
 
 user_dir = '/data/1/sporty/users/'
 friends_dir = '/data/1/sporty/friends/'
 uids = [f for f in listdir(user_dir) if isfile(join(user_dir,f))]
 
-print len(uids)
+# print len(uids)
 # uids = [111397971,1115884644,111636637,1118674309,1120500210,112091139,112203950,1122204780]
 
-users = u.api(uids[-100:])
-users.getMostSimilarFriend(user_dir, friends_dir)
+api = u.api(uids)
+
+# api.load(user_dir)
+# api.loadFriends(friends_dir)
+
+# for user in api.users:
+#     m = re.match("([^,]+),\s+([A-Za-z]{2,})", user['location'])
+#     if m:
+#         print m.group(1, 2)
+
+api.getMostSimilarFriend(user_dir, friends_dir)
