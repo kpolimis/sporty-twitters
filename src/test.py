9a323981710edf7c547cff5ff27272610da790ec
Virgile Landeiro <vlandeir@hawk.iit.edu>
2014-07-15 16:09:19 -0500

    changed test script

--- src/test.py
+++ src/test.py
@@ -11,7 +11,7 @@ uids = [f for f in listdir(user_dir) if isfile(join(user_dir,f))]
 # print len(uids)
 # uids = [111397971,1115884644,111636637,1118674309,1120500210,112091139,112203950,1122204780]
 
-api = u.api(uids)
+api = u.api(uids[:1000])
 
 # api.load(user_dir)
 # api.loadFriends(friends_dir)
@@ -21,4 +21,4 @@ api = u.api(uids)
 #     if m:
 #         print m.group(1, 2)
 
-api.getMostSimilarFriend(user_dir, friends_dir)
+api.buildSimilarityMatrix(user_dir, friends_dir)
